bdcad610f5b0d4906e1ad50f9a8626962623b8fd
Subhajit121 <pubgraj.tornado1216@gmail.com>
2026-07-15 17:42:47 +0530

    Step 4 - Extract PDF metadata

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from src.parser.pdf_reader import PDFReader
+from src.extractor.metadata_extractor import MetadataExtractor
 
 reader = PDFReader()
 
 document = reader.open("samples/pdf/spdf4.pdf")
+metadata = MetadataExtractor().extract(document)
 
-print("PDF opened successfully!")
+print("\n========== PDF Metadata ==========\n")
 
-print(document)
+for key, value in metadata.items():
+    print(f"{key:15}: {value}")
---
+++ src/extractor/metadata_extractor.py
@@ -0,0 +1,11 @@
+class MetadataExtractor:
+    """
+    Extract metadata from a PDF document.
+    """
+
+    def extract(self, document):
+        """
+        Returns PDF metadata.
+        """
+
+        return document.metadata
